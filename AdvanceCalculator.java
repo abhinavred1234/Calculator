afbaf40c1d2412de668bb996adaf98c97b22ea4f
abhinavred1234 <121377910+abhinavred1234@users.noreply.github.com>
2024-05-09 12:11:35 +0530

    Add files via upload
    This is a basic Calculator made in java

---
+++ AdvanceCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class AdvanceCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter the operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        double result = 0;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2!= 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.println("The final result: " + num1 + " " + op + " " + num2 + " = " + result);
+    }
+}
